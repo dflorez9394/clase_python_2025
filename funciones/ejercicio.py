a20b3082f5596b4285823089cb10e206227f7d09
dflorez <daniel0223@hotmail.es>
2026-01-28 19:29:23 -0500

    fix elementos

--- funciones/ejercicio.py
+++ funciones/ejercicio.py
@@ -1,11 +1,26 @@
+def validar_sueldos(sueldos):
+    if len(sueldos) == 0:
+        print("No hay sueldos registrados")
+        return False#si sueldos es vacio o no tiene sueldos retorna un FALSE
+    return True#Si tiene sueldos retorna un true
 def agregar_sueldo():
     sueldo =float(input("Ingrese el sueldo: "))
     return sueldo
 def mostrar_sueldos(sueldos):
-    contador = 1
-    for sueldo in sueldos:
-        print(f"Sueldo {contador}: {sueldo}")
-        contador += 1
+    flagValidarSueldo = validar_sueldos(sueldos)
+    print(f"Flag validar sueldo: {flagValidarSueldo}")
+    if flagValidarSueldo:
+        contador = 1
+        for sueldo in sueldos:
+            print(f"Sueldo {contador}: {sueldo}")
+            contador += 1
+        
+def mostrar_sueldo_mayor_menor(sueldos):
+    if len(sueldos) == 0:
+        print("No hay sueldos registrados")
+    else:
+        print(f"Sueldo mayor: {max(sueldos)}")
+        print(f"Sueldo menor: {min(sueldos)}")
 
 def menu():
     sueldos = []
@@ -14,7 +29,7 @@ def menu():
         print("Menu principal infinity")
         print("1) Agregar Sueldo del Docente")
         print("2) Mostrar Sueldo de docentes")
-        #print("3) Mostrar el Sueldo mayor y menos")
+        print("3) Mostrar el Sueldo mayor y menor")
         print("4) salir")
         opcion=int(input("Ingrese una opcion: "))
         if opcion == 1:
@@ -32,4 +47,4 @@ def menu():
     
 #El corazon de la ejecucion o centralizado de la aplicacion 
 if __name__ == "__main__":
-    menu() 
+    menu()
