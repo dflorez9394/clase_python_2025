e9d24285326240352f0a77147b2144f9764fdcaa
daniel florez <danielflorez@192.168.81.13>
2026-02-11 18:08:27 -0500

    xommit

--- pokedex_poo/main.py
+++ pokedex_poo/main.py
@@ -1,23 +1,10 @@
 import requests as rq# una libreria que nos trae funcionalidades a nuestro codigo
 
-from Pokemon import Pokemon
-from poke_api_client import PokeAPIClient
+from pokedex_app import PokedexApp
 
 
 def main():
-    nombre_pokemon = input("Ingrese el nombre del pokemon: ").strip().lower()
-    url = f"https://pokeapi.co/api/v2/pokemon/{nombre_pokemon}"
-    response = rq.get(url)
-    if response.status_code != 200:
-        print("El nombre del pokemon no existe")
-        return
-    data = response.json()
-    print("Nombre del pokemon es: ", data["name"])
-    print("ID del pokemon es: ", data["id"])
+    app = PokedexApp()
+    app.run()
 if __name__ == '__main__':
-    pokemon = input("Ingrese el nombre del pokemon: ")
-    pokemon_list =  PokeAPIClient().get_by_name(pokemon)
-    pokemon_list.show_pokemon()
-   # main()
-
-#main => le corazon del proyecto que va ejecutar cada clase o metodo para solucionar el problema
+    main()
--- pokedex_poo/poke_api_client.py
+++ pokedex_poo/poke_api_client.py
@@ -4,22 +4,33 @@ from Pokemon import Pokemon
 
 
 class PokeAPIClient:
+    #CONSTANTES
+    BASE_URL = "https://pokeapi.co/api/v2"
 
 
     def get_by_name(self,name: str):
         name = name.strip().lower()#normalizar el name
-        url = f"https://pokeapi.co/api/v2/pokemon/{name}"
-        response = requests.get(url)
-        if response.status_code != 200:
-            print("error del servicio")
-        data = response.json()
-        #Debemos sacar los nombres de la lista
-        for type in data["types"]:
-            habilidad = type["type"]["name"]
-            print(f"Pokemon {type}")
-            print(f"hanilidad {habilidad}")
-        return Pokemon(
-            pokemon_id=data["id"],
-            name=data["name"],
-            types=data["types"]["type"]["name"],#['ground' , 'rock']
-        )
+        url = f"{self.BASE_URL}/pokemon/{name}"
+        try:
+            response = requests.get(url)
+            if response.status_code != 200:
+                raise ValueError(f"El nombre del pokemon no existe: {name}")
+            data = response.json()
+            # Debemos sacar los nombres de la lista
+            # forma 1
+            types_pokemon = []
+            # for type in data["types"]:
+            #    types_pokemon.append(type["type"]["name"])
+            # forma 2
+            # types_pokemon= [t["type"]["name"] for t in data["types"]]
+            return Pokemon(
+                pokemon_id=data["id"],
+                name=data["name"],
+                # types=types_pokemon
+                # types=data["types"]["type"]["name"],#['ground' , 'rock']
+                types=[t["type"]["name"] for t in data["types"]],
+            )
+        except requests.exceptions.RequestException as e:
+           raise ConnectionError(f"Error al conectarse a el API {self.BASE_URL}")
+        finally:
+            print("Se ejecuto el metodo get_by_name")
